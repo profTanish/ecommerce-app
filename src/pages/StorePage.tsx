f846143e8efc77e4fb814991058f6c22d7b025b3
profTanish <profile.tanish@gmail.com>
2025-03-23 22:26:54 +0530

    Finished store page style, added all sections

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -1,3 +1,4 @@
+import CtaSection from "../components/CtaSection";
 import Product from "../components/Product";
 
 const StorePage = () => {
@@ -13,56 +14,71 @@ const StorePage = () => {
                     Shoptidy
                 </h1>
             </div>
-            <div className="py-16 grid grid-cols-[1fr_3fr] max-w-container mx-auto">
-                <div className="flex flex-col gap-8">
-                    <div className="sort-container">
-                        <h3 className="heading-tertiary mb-2.5">Sort By</h3>
-                        <select className="py-2 px-4 rounded-md w-56 bg-transparent border border-darkGray cursor-pointer text-sm">
-                            <option className="" value="name-asc">
-                                Name Ascending A-Z
-                            </option>
-                            <option value="name-desc">Name Descending Z-A</option>
-                            <option value="price-desc">Price High - Low</option>
-                            <option value="price-asc">Price Low - High</option>
-                        </select>
-                    </div>
 
-                    <div>
-                        <h3 className="heading-tertiary mb-2.5">Category</h3>
-                        <ul className="flex flex-col gap-2.5 items-start">
-                            {[
-                                "all",
-                                "clothes",
-                                "electronics",
-                                "furniture",
-                                "shoes",
-                                "miscellaneous",
-                                "others",
-                            ].map((category) => (
-                                <li key={category}>
-                                    <button
-                                        className="category-button hover:text-darkSlate"
-                                        value={category}
-                                    >
-                                        {category.charAt(0).toUpperCase() + category.slice(1)}
-                                    </button>
-                                </li>
-                            ))}
-                        </ul>
+            <div className="max-w-container mx-auto">
+                <div className="py-16 grid grid-cols-[1fr_3fr]">
+                    <div className="flex flex-col gap-8">
+                        <div className="sort-container">
+                            <h3 className="heading-tertiary mb-2.5">Sort By</h3>
+                            <select className="py-2 px-4 rounded-md w-56 bg-transparent border border-darkGray cursor-pointer text-sm">
+                                <option className="" value="name-asc">
+                                    Name Ascending A-Z
+                                </option>
+                                <option value="name-desc">Name Descending Z-A</option>
+                                <option value="price-desc">Price High - Low</option>
+                                <option value="price-asc">Price Low - High</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <h3 className="heading-tertiary mb-2.5">Category</h3>
+                            <ul className="flex flex-col gap-2.5 items-start">
+                                {[
+                                    "all",
+                                    "clothes",
+                                    "electronics",
+                                    "furniture",
+                                    "shoes",
+                                    "miscellaneous",
+                                    "others",
+                                ].map((category) => (
+                                    <li key={category}>
+                                        <button
+                                            className="category-button hover:text-darkSlate"
+                                            value={category}
+                                        >
+                                            {category.charAt(0).toUpperCase() + category.slice(1)}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h3 className="heading-tertiary mb-2.5">Price</h3>
+                            <p className="mb-2.5">$44</p>
+                            <input type="range" min="0" max="99999" step="5" />
+                        </div>
                     </div>
-                    <div>
-                        <h3 className="heading-tertiary mb-2.5">Price</h3>
-                        <p className="mb-2.5">$44</p>
-                        <input type="range" min="0" max="99999" step="5" />
+                    <div className="grid grid-cols-4 gap-4">
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
                     </div>
                 </div>
-                <div className="grid grid-cols-4 gap-4">
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
+
+                <section className="py-16 space-y-5">
+                    <h2 className="heading-secondary">Related Products</h2>
+                    <div className="grid grid-cols-3 gap-10">
+                        <Product />
+                        <Product />
+                        <Product />
+                    </div>
+                </section>
+
+                <CtaSection />
             </div>
         </div>
     );
