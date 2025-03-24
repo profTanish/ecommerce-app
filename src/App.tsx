0b67aee2b602560d48771d79a71c2e34f56c7d99
profTanish <profile.tanish@gmail.com>
2025-03-24 16:34:32 +0530

    Added types for supabase

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,42 @@ import PageNotFound from "./pages/PageNotFound";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import StorePage from "./pages/StorePage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+ 
+ const queryClient = new QueryClient({
+   defaultOptions: {
+     queries: {
+       staleTime: 0,
+     },
+   },
+ });
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={
-            <ProtectedRoute>
-              <AppLayout />
-            </ProtectedRoute>
-          }
-          >
-          <Route index element={<Navigate replace to="home" />} />
-          <Route path="home" element={<Homepage />} />
-          <Route path="store" element={<StorePage />} />
-          <Route path="about" element={<About />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+       <ReactQueryDevtools initialIsOpen={false} />
+ 
+       <BrowserRouter>
+         <Routes>
+           <Route
+             element={
+               <ProtectedRoute>
+                 <AppLayout />
+               </ProtectedRoute>
+             }
+           >
+             <Route index element={<Navigate replace to="home" />} />
+             <Route path="home" element={<Homepage />} />
+             <Route path="store" element={<StorePage />} />
+             <Route path="about" element={<About />} />
+           </Route>
 
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+           <Route path="login" element={<Login />} />
+           <Route path="*" element={<PageNotFound />} />
+         </Routes>
+       </BrowserRouter>
+     </QueryClientProvider>
   );
 }
 
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../database.types";
+
 const supabaseUrl = "https://akyztpchcdqpamqgattl.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
