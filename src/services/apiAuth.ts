5feaf683788c10b7ab4d5d368b56401aa8bd88b0
profTanish <profile.tanish@gmail.com>
2025-03-28 15:14:47 +0530

    Added logout api function

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -20,12 +20,17 @@ export async function login({ email, password }: LoginProps) {
 export async function getCurrentUser() {
     const { data: session } = await supabase.auth.getSession();
     if (!session) return null;
-  
+
     const { data, error } = await supabase.auth.getUser();
-  
+
     console.log(data);
-  
+
     if (error) throw new Error(error.message);
-  
+
     return data?.user;
-  }
+}
+
+export async function logout() {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw new Error(error.message);
+}
