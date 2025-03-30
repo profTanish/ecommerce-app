d2d0f70cf72235f3bc351c766cca534dea4dc1fb
profTanish <profile.tanish@gmail.com>
2025-03-30 13:58:09 +0530

    Breadcrumb component is now dynamic, accepts items

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -6,27 +6,42 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const Breadcrumb = () => {
+type BreadcrumbItem = {
+    name: string;
+    link: string;
+};
+
+type BreadcrumbProps = {
+    items: BreadcrumbItem[];
+};
+
+const Breadcrumb = ({ items }: BreadcrumbProps) => {
+    if (!items) return null;
+
     return (
         <ShadcnBreadCrumb>
             <BreadcrumbList>
-                <BreadcrumbItem>
-                    <BreadcrumbLink asChild>
-                        <Link to="/store">Store</Link>
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                    <BreadcrumbLink asChild>
-                        <Link to="/store">Clothes</Link>
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                    <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-                </BreadcrumbItem>
+                {items.map((item, i) => {
+                    const isLastItem = items.length === i + 1;
+
+                    return (
+                        <React.Fragment key={item?.link}>
+                            <BreadcrumbItem>
+                                {!isLastItem ? (
+                                    <BreadcrumbLink asChild>
+                                        <Link to={item?.link}>{item.name}</Link>
+                                    </BreadcrumbLink>
+                                ) : (
+                                    <BreadcrumbPage>{item.name}</BreadcrumbPage>
+                                )}
+                            </BreadcrumbItem>
+                            {!isLastItem && <BreadcrumbSeparator />}
+                        </React.Fragment>
+                    );
+                })}
             </BreadcrumbList>
         </ShadcnBreadCrumb>
     );
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -3,7 +3,13 @@ import Broadcrumb from "../components/Breadcrumb";
 const ProductDetails = () => {
     return (
         <div>
-            <Broadcrumb />
+            <Broadcrumb
+                items={[
+                    { name: "Store", link: "/store" },
+                    { name: "Clothes", link: "/store/clothes" },
+                    { name: "Black Hoodie", link: "" },
+                ]}
+            />
         </div>
     );
 };
