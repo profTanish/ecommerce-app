71da25158da3ccc1d3b7bffcf315336ef761c65e
profTanish <profile.tanish@gmail.com>
2025-03-31 11:50:44 +0530

    Added style for cart overview

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,27 +1,32 @@
 import { useMoveBack } from "@/hooks/useMoveBack";
 import { HiArrowUturnLeft } from "react-icons/hi2";
 import CartItem from "./CartItem";
+import CartOverview from "./CartOverview";
 
 const Cart = () => {
     const moveBack = useMoveBack();
 
     return (
-        <div className="max-w-container mx-auto py-16">
-            <button
-                onClick={moveBack}
-                className="flex items-center gap-2.5 text-textGray hover:text-darkSlate"
-            >
-                <span>
-                    <HiArrowUturnLeft />
-                </span>
-                Go Back
-            </button>
+        <div className="max-w-container mx-auto py-16 flex justify-between">
+            <div>
+                <button
+                    onClick={moveBack}
+                    className="flex items-center gap-2.5 text-textGray hover:text-darkSlate"
+                >
+                    <span>
+                        <HiArrowUturnLeft />
+                    </span>
+                    Go Back
+                </button>
 
-            <h2 className="heading-tertiary mt-5 mb-8">Shopping Cart</h2>
+                <h2 className="heading-tertiary mt-5 mb-8">Shopping Cart</h2>
 
-            <ul>
-                <CartItem />
-            </ul>
+                <ul>
+                    <CartItem />
+                </ul>
+            </div>
+
+            <CartOverview />
         </div>
     );
 };
---
+++ src/features/cart/CartOverview.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+
+const CartOverview = () => {
+    return (
+        <div className="bg-lightGray p-8 rounded-md max-w-[26rem] w-full">
+            <h3 className="heading-tertiary">Order Details</h3>
+            <ul className="space-y-2.5 my-5">
+                <li className="text-textGray flex items-center justify-between">
+                    <p>Subtotal</p>
+                    <p>$44.99</p>
+                </li>
+                <li className="text-textGray flex items-center justify-between">
+                    <p>Shipping</p>
+                    <p>$10.00</p>
+                </li>
+                <li className="text-textGray flex items-center justify-between">
+                    <p>Tax</p>
+                    <p>7%</p>
+                </li>
+                <li className="pt-5 text-textGray font-semibold flex items-center justify-between border-t border-darkGray">
+                    <p>Grand Total</p>
+                    <p>$100.00</p>
+                </li>
+            </ul>
+            <Button className="w-full">Checkout</Button>
+        </div>
+    );
+};
+
+export default CartOverview;
