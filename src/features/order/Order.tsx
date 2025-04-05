b214584aed2a82a6ffe754188325bb25cee60c13
profTanish <profile.tanish@gmail.com>
2025-04-06 03:41:01 +0530

    Changed image size from 7.5rem to 5rem

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -22,11 +22,11 @@ const Order = () => {
                     <ul className="max-h-96 overflow-scroll mb-5">
                         <li className="flex gap-16 justify-between items-center py-5 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-lightGray">
                             <div className="flex items-center gap-4">
-                                <div className="max-w-[7.5rem] rounded-md overflow-hidden">
-                                    <img className="w-full h-full" src="test" alt="bike" />
+                                <div className="max-w-[5] max-h-[5rem] rounded-md overflow-hidden">
+                                    <img className="w-full h-full" src="/test.jpg" alt="bike" />
                                 </div>
                                 <p className="font-bold">1x</p>
-                                <p>Classic Red Jogger Sweatpants</p>>
+                                <p>Classic Red Jogger Sweatpants</p>
                             </div>
 
                             <p className="font-bold">$48.99</p>
