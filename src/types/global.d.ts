dbc6a685539fc3203fcfb3f1b5a4a41709c9063b
profTanish <profile.tanish@gmail.com>
2025-04-08 12:29:13 +0530

    Added types for CartProduct, Address and Customer

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,6 +3,29 @@ import type { Database } from "../types/database.types";
 declare global {
   type product = Database["public"]["Tables"]["products"]["Row"];
 
+  type Address = {
+    phone: string;
+    city: string;
+    street: string;
+  };
+
+  type Customer = {
+    address: Address | null;
+    created_at: string;
+    email: string | null;
+    fullName: string | null;
+    id: number;
+  };
+
+  type CartProduct = {
+    productId: number;
+    name: string;
+    category: string;
+    price: number;
+    image: string;
+    totalPrice: number;
+    quantity: number;
+  };
 
   type Option = {
     value: string;
