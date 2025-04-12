a665bcf5da45c9b0259b15ba7e1bbbfded8b970d
profTanish <profile.tanish@gmail.com>
2025-04-12 13:06:30 +0530

    Changed background color for skeleton product item

--- src/components/SkeletonProduct.tsx
+++ src/components/SkeletonProduct.tsx
@@ -3,12 +3,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 const SkeletonProduct = () => {
     return (
         <div className="flex flex-col space-y-2.5">
-            <Skeleton className="h-[235.5px] w-[235.5px] rounded-lg bg-lightGray" />
-            <Skeleton className="h-6 w-[100px] rounded-full bg-lightGray" />
-            <Skeleton className="h-8 w-[80px] rounded-full bg-lightGray" />
+            <Skeleton className="h-[235.5px] w-[235.5px] rounded-lg bg-darkGray" />
+            <Skeleton className="h-6 w-[100px] rounded-full bg-darkGray" />
+            <Skeleton className="h-8 w-[80px] rounded-full bg-darkGray" />
             <div className="flex gap-2.5">
-                <Skeleton className="h-9 w-full rounded-full bg-lightGray" />
-                <Skeleton className="h-9 w-full rounded-full bg-lightGray" />
+                <Skeleton className="h-9 w-full rounded-full bg-darkGray" />
+                <Skeleton className="h-9 w-full rounded-full bg-darkGray" />
             </div>
         </div>
     );
